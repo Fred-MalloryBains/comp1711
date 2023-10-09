be15f2384842c04e85f7534ea6d84b1cece38551
Fred-MalloryBains <146655395+Fred-MalloryBains@users.noreply.github.com>
2023-10-09 09:54:19 +0000

    end of week2 session1

--- week_1_c/first.c
+++ week_1_c/first.c
@@ -1,17 +1,99 @@
 #include <stdio.h>
+#include <math.h>
 
-int main() {
+int helloWorld () {
+    printf("Hello World!\n ");
+    return 0;
+}
+
+int summation (){
     float a;
     float b; 
+
     float c;
 
-    printf ("enter number a:");
-    scanf ("%f", &a);
-    printf ("enter number b:");
-    scanf ("%f", &b);
-
+    printf ("enter two numbers: ");
+    scanf ("%f", &a );
+    scanf ("%f", &b );
     c = a + b;
-    printf ("result is %lf\n", c);    
+    printf ("The result is %f\n", c);
+    return 0;
+}
+
+int areaOfCircle () {
+
+    float radius;
+    float area;
+
+   // used math import 
+   // M_PI is pi constant
+
+    printf ("enter the radius: ");
+    scanf ("%f", &radius);
+
+    // pi r^2
+
+    area = radius * radius * M_PI;
+
+    printf ("THe area is %f\n", area);
+
+    return 0;
+
+
+
+}
+
+int swapVariables (){
+    // not doing it
+}
+
+int fareinheit (){
+    float temperature;
+    scanf ("%f",&temperature);
+
+    temperature = (temperature - 32) * (5/6);
+    printf ("The result is %f\n", temperature);
+    return 0;
+
+
+
+}
+
+int oddOrEven (){
+    int a;
+    printf ("enter a number: ");
+    scanf ("%d",&a);
+    a = a % 2;
+
+
+    if (a == 0){
+        printf("even\n");
+    } else {
+        printf ("odd\n");
+    }
+}
+int factorial_recursion (int number){
+    if (number < 2){
+        return number;
+    } else {
+        return (number * factorial_recursion((number-1)));
+    }
+}
+
+int factorial () {
+    int number;
+    int result;
+
+    printf ("Enter a number: ");
+    scanf ("%d", &number);
+    result = factorial_recursion (number);
+    printf ("the result is %d\n", result);
+
+
+}
+int main() {
+    fareinheit();
+    return 0;
 
     
 }
